73f9ff335f26ee5acf3dbac03abea9e7e6482eee
ilittleangel <angel.rojo.perez@gmail.com>
2018-11-03 10:16:33 +0100

    fix: filter indices with date pattern at the end

--- utils/elastic.py
+++ utils/elastic.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import re
 import requests
 from requests.auth import HTTPBasicAuth
 from datetime import datetime
@@ -8,6 +9,7 @@ from settings import ES_NODE, ES_PATTERN, ES_USER, ES_PASS
 
 
 logger = logging.getLogger('watcher')
+date_patter = re.compile(r'(20\d{2})(\d{2})(\d{2})')
 
 
 def get_newest_index():
@@ -18,10 +20,14 @@ def get_newest_index():
         rq = requests.get(f"{ES_NODE}/_cat/indices", auth=HTTPBasicAuth(ES_USER, ES_PASS))
         logger.debug(f"Indices found:\n\r{rq.text}")
         rq.raise_for_status()
-        indices = sorted(
-            [line.split()[2] for line in rq.text.splitlines() if line.split()[2].startswith(ES_PATTERN)],
-            key=lambda x: datetime.strptime(x[-len(ES_PATTERN)+1:], '%Y%m%d'),
-            reverse=True)
+
+        indices_with_pattern = [line.split()[2] for line in rq.text.splitlines()
+                                if line.split()[2].startswith(ES_PATTERN) and
+                                date_patter.match(line.split()[2][-len(ES_PATTERN)+1:])]
+
+        indices = sorted(indices_with_pattern,
+                         key=lambda x: datetime.strptime(x[-len(ES_PATTERN)+1:], '%Y%m%d'),
+                         reverse=True)
 
         logger.debug(f"Indices found with pattern '{ES_PATTERN}': {len(indices)}")
         logger.info(f"Last index: {indices[0]}")
