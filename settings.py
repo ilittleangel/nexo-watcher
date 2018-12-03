e444614bb20085b64bb2e5ba20e0588e73366734
ilittleangel <angel.rojo.perez@gmail.com>
2018-12-03 22:00:52 +0100

    api logs notification

--- settings.py
+++ settings.py
@@ -8,15 +8,15 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 
 # elastic
-elastic = config['ELASTIC']
-ES_NODE = elastic['esnode']
-ES_PATTERN = elastic['pattern'].lower()
-ES_RANGE_WINDOW = elastic.getint('window_minutes', fallback=5)
+elastic_cfg = config['ELASTIC']
+ES_NODE = elastic_cfg['esnode']
+ES_PATTERN = elastic_cfg['pattern'].lower()
+ES_RANGE_WINDOW = elastic_cfg.getint('window_minutes', fallback=5)
 ES_USER = ""
 ES_PASS = ""
 try:
-    ES_USER = elastic['user']
-    ES_PASS = elastic['pass']
+    ES_USER = elastic_cfg['user']
+    ES_PASS = elastic_cfg['pass']
 except KeyError:
     pass
 
@@ -24,12 +24,18 @@ except KeyError:
 LOG_LEVEL = config['LOGGING']['level']
 
 # watcher
-watcher = config['WATCHER']
-SLEEP_MINUTES = watcher.getint('sleep_minutes', fallback=60)
-NOTIFICATION_CHANNELS = watcher['notification_channels'].replace(' ', '').split(',')
-SLACK_HOOK = watcher['slack_web_hook']
-SLACK_CHANNEL = watcher['slack_channel']
-KIBANA = watcher['kibana']
-ACTIVATE_ALARM_GOOD = watcher.getboolean('activate_alarm_good', fallback=False)
-MAX_THRESHOLD = watcher.getint('max_threshold', fallback=90)
-MIN_THRESHOLD = watcher.getint('min_threshold', fallback=1)
+watcher_cfg = config['WATCHER']
+SLEEP_MINUTES = watcher_cfg.getint('sleep_minutes', fallback=60)
+NOTIFICATION_CHANNELS = watcher_cfg['notification_channels'].replace(' ', '').split(',')
+SLACK_HOOK = watcher_cfg['slack_web_hook']
+SLACK_CHANNEL = watcher_cfg['slack_channel']
+KIBANA = watcher_cfg['kibana']
+ACTIVATE_ALARM_GOOD = watcher_cfg.getboolean('activate_alarm_good', fallback=False)
+MAX_THRESHOLD = watcher_cfg.getint('max_threshold', fallback=90)
+MIN_THRESHOLD = watcher_cfg.getint('min_threshold', fallback=1)
+
+# api logs
+api_cfg = config['API_LOGS']
+HOST_URL = api_cfg['host_url']
+PATH_ERROR = api_cfg['full_path_error']
+PATH_WARN = api_cfg['full_path_warning']
--- utils/notifications.py
+++ utils/notifications.py
@@ -1,9 +1,11 @@
+import json
 import logging
 import requests
 import sys
 
 from settings import ES_RANGE_WINDOW, NOTIFICATION_CHANNELS
 from settings import SLACK_HOOK, KIBANA, SLACK_CHANNEL, ACTIVATE_ALARM_GOOD
+from settings import HOST_URL, PATH_ERROR, PATH_WARN
 
 
 logger = logging.getLogger('watcher')
@@ -25,17 +27,34 @@ def slack(msg, kind):
         'very bad': ":scream:",
         'good': ":grin:",
     }
+    message = f"""
+        {msg}
+        Information about errors: {api_logs()}
+    """
     payload = {
         "channel": f"#{SLACK_CHANNEL}",
         "username": "webhookbot",
         "icon_emoji": emoji.get(kind),
-        "text": msg
+        "text": message
     }
     res = requests.post(SLACK_HOOK, json=payload)
     if res.status_code == 200:
         logger.info(f"Success Slack notification: type -> `{kind}`")
     else:
         logger.error(f"Fail notification: {res.text}")
+
+
+def api_logs():
+    try:
+        rq_errors = requests.get(url=f"{HOST_URL}{PATH_ERROR}")
+        # rq_errors.raise_for_status()
+        errors = json.dumps(rq_errors.json(), indent=4)
+        if not errors:
+            rq_warns = requests.get(url=f"{HOST_URL}{PATH_WARN}")
+            return json.dumps(rq_warns.json(), indent=4)
+        return errors
+    except requests.exceptions.RequestException as re:
+        logger.error(f"API LOGS FAILED: see trace errors: {re}")
 
 
 def notify(hits, kind):
